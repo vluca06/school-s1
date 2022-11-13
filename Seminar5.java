b71d93dc7d98fa0fbdf9c67d65dc918293064821
vluca06 <114497281+vluca06@users.noreply.github.com>
2022-11-13 13:35:50 +0200

    Add files via upload

---
+++ Seminar5.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.seminar5;
+import static com.mycompany.seminar5.MergeSort.printArray;
+
+/**
+ *
+ * @author vluca
+ */
+public class Seminar5 {
+    
+	public static void main(String args[])
+	{
+		int arr[] = {12, 11, 13, 5, 6, 7};
+
+		System.out.println("Given Array");
+		printArray(arr);
+
+		MergeSort ob = new MergeSort();
+		ob.sort(arr, 0, arr.length-1);
+
+		System.out.println("\nSorted array");
+		printArray(arr);
+	}
+    
+}
+
+class MergeSort
+{
+	void merge(int arr[], int l, int m, int r)
+	{
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		int L[] = new int [n1];
+		int R[] = new int [n2];
+
+		for (int i=0; i<n1; ++i)
+			L[i] = arr[l + i];
+		for (int j=0; j<n2; ++j)
+			R[j] = arr[m + 1+ j];
+
+		int i = 0, j = 0;
+
+		int k = l;
+		while (i < n1 && j < n2)
+		{
+			if (L[i] <= R[j])
+			{
+				arr[k] = L[i];
+				i++;
+			}
+			else
+			{
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1)
+		{
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2)
+		{
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	void sort(int arr[], int l, int r)
+	{
+		if (l < r)
+		{
+			int m = (l+r)/2;
+
+			sort(arr, l, m);
+			sort(arr , m+1, r);
+
+			merge(arr, l, m, r);
+		}
+	}
+
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+
+}
+
